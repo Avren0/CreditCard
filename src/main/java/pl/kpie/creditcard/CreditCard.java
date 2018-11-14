c602380ba3c8264b5d417d406a0506470f175a48
bartek <gekarez@gmail.com>
2018-11-14 23:44:33 +0100

    Add method withdraw()

--- src/main/java/pl/kpie/creditcard/CreditCard.java
+++ src/main/java/pl/kpie/creditcard/CreditCard.java
@@ -54,6 +54,27 @@ class CreditCard {
         return this.blocked;
     }
 
+    public void withdraw(BigDecimal money) {
+        if (isWithdrawOverTheLimit(money))
+            throw new NotEnoughMoneyException();
+
+        if (isNotEnoughMoney(money))
+            throw new NotEnoughMoneyException();
+
+        if (isBlocked())
+            throw new TransactionOnBlockedCardException();
+
+        balance = balance.subtract(money);
+    }
+
+    private boolean isNotEnoughMoney(BigDecimal money) {
+        return money.compareTo(balance) > 0;
+    }
+
+    private boolean isWithdrawOverTheLimit(BigDecimal money) {
+        return money.compareTo(limit) > 0;
+    }
+
 
     public void repay(BigDecimal money) {
         if (money.compareTo(BigDecimal.ZERO) < 0) {
